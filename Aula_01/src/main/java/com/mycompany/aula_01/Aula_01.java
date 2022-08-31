ee7363adccd65c7f1369b2bd69422f3e96372f5b
Felipe <santospinheiro627@gmail.com>
2022-08-30 21:57:08 -0300

    big bang

---
+++ Aula_01/src/main/java/com/mycompany/aula_01/Aula_01.java
@@ -0,0 +1,35 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Project/Maven2/JavaApp/src/main/java/${packagePath}/${mainClassName}.java to edit this template
+ */
+
+package com.mycompany.aula_01;
+
+import javax.swing.JOptionPane;
+
+/**
+ *
+ * @author Felipe
+ */
+public class Aula_01 {
+
+    public static void main(String[] args) {
+        //System.out.println("Hello World!");                                                                                                                                                                                                                                                                                                                                                                                          
+        // criando objeto do tipo 'Sobrecarga' para ter acesso a essa classe
+        
+        Sobrecarga Sobrecarga = new Sobrecarga();
+        
+        double n1, n2, n3, maior;
+        //primeiro metodo;
+        
+        
+        n1 = Double.parseDouble(JOptionPane.showInputDialog("Digite O primeiro número:"));
+        n2 = Double.parseDouble(JOptionPane.showInputDialog("Digite O segundo número:"));
+        
+        JOptionPane.showMessageDialog(null,n1);
+        JOptionPane.showMessageDialog(null,n2);
+       
+        
+        
+    }
+}
