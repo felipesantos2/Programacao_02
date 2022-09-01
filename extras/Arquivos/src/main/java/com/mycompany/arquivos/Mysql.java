36dd1c5abdf7406bfee78cad58ef0070d30e238e
Felipe <santospinheiro627@gmail.com>
2022-08-31 22:01:46 -0300

    con java mysql

--- extras/Arquivos/src/main/java/com/mycompany/arquivos/Mysql.java
+++ extras/Arquivos/src/main/java/com/mycompany/arquivos/Mysql.java
@@ -4,10 +4,35 @@
  */
 package com.mycompany.arquivos;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+import javax.swing.JOptionPane;
+
 /**
  *
  * @author Felipe
  */
-public class Mysql {
+public class Mysql{
     
+    //coneção com database mysql
+
+    public static Connection ConectaDB (){
+        
+        Connection conn = null;
+        
+        try {
+            String url = "jdbc:mysql://localhost:3306/sakila?user=root&password=";
+            conn = DriverManager.getConnection(url);   
+            
+        } catch (SQLException e){
+            JOptionPane.showMessageDialog(null," O erro está na classe de conexão Mysql " + e.getMessage());
+            
+        }
+        return conn;
+    }
+    
+     
 }
