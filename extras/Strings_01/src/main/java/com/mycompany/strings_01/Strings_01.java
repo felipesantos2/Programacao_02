c9e032a676dce3d0f551a9194b6c332a023866eb
Felipe <santospinheiro627@gmail.com>
2022-09-12 23:28:48 -0300

    lidando com strings em java

--- extras/Strings_01/src/main/java/com/mycompany/strings_01/Strings_01.java
+++ extras/Strings_01/src/main/java/com/mycompany/strings_01/Strings_01.java
@@ -1,8 +1,3 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Project/Maven2/JavaApp/src/main/java/${packagePath}/${mainClassName}.java to edit this template
- */
-
 package com.mycompany.strings_01;
 
 /**
@@ -12,6 +7,62 @@ package com.mycompany.strings_01;
 public class Strings_01 {
 
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+        
+        //charAt();substring();
+        
+       String texto = "Frase de testes";
+       
+//        for(int i = 0; i < texto.length(); i++) {
+//            char caracter = texto.charAt(i);
+//            System.out.println(caracter);
+//        }
+//   
+
+
+    
+    
+//    String sustr_1 = texto.substring(4);
+//    String sustr_2 = texto.substring(1, 7);
+//    
+//    System.out.println(sustr_1);
+//    System.out.println(sustr_2);
+
+
+
+    //equals();
+    
+//    String texto_2 = "Nova Frase";
+//    
+//    if(texto.equals(texto_2)) {
+//        System.out.println("OS textos são iguais"); 
+//    }
+//    if(texto.equals(texto_2) == false) {
+//        System.out.println("OS textos não são iguais"); 
+//    }
+//    
+
+    
+    //concat
+        String um = "Felipe ";
+        String dois = "Pinheiro";
+
+        String jucao = um.concat(dois);
+        System.out.println(jucao.concat(" dos Santos"));
+        
+    //indexOf
+//        int posicao = um.indexOf('i');
+//        int posicao2 = um.lastIndexOf('i');
+//        System.out.println(posicao);
+//        System.out.println(posicao);
+        
+    //inEmpty(); 
+    if(um.isEmpty()){
+        System.out.println("Sim");
+    } else {
+        System.out.println("Não");
     }
+    
+    }
+    
+    
 }
