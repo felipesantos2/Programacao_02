d1477cfea7299735ae1eb094048d0172250a72fc
Felipe pinheiro dos Santos <santospinheiro627@gmail.com>
2022-09-16 14:06:27 -0300

    Update MAPA.java

--- MAPA/src/main/java/com/mycompany/mapa/MAPA.java
+++ MAPA/src/main/java/com/mycompany/mapa/MAPA.java
@@ -1,7 +1,3 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Project/Maven2/JavaApp/src/main/java/${packagePath}/${mainClassName}.java to edit this template
- */
 
 package com.mycompany.mapa;
 
@@ -13,44 +9,62 @@ import javax.swing.JOptionPane;
  */
 public class MAPA {
     
-    
+    //FELIPE PINHEIRO DOS SANTOS 
+    //16/09/2022  
 
     public static void main(String[] args) {
         // declarando as variaveis;
         int a ,b , c;
         double delta, x1, x2;
         
-        a = Integer.parseInt(JOptionPane.showInputDialog("Informe o valor de A : "));
-        b = Integer.parseInt(JOptionPane.showInputDialog("Informe o valor de B : "));
-        c = Integer.parseInt(JOptionPane.showInputDialog("Informe o valor de C : "));
-       // System.out.println("O valor de a é: "+ a +"-" + b + "-" + c);
-        
-        if(a == 0) {
-            JOptionPane.showMessageDialog(null, "O número correspondente a letra `A` tem que ser diferente de ZERO -> "
-                    + " a != 0 ");
-        }
-   
-        if(a != 0){
+        try{
             
-            delta = (b * b) - 4 * a * c;
+            a = Integer.parseInt(JOptionPane.showInputDialog("Informe o valor de A que seja diferente de 0 : "));
+            b = Integer.parseInt(JOptionPane.showInputDialog("Informe o valor de B : "));
+            c = Integer.parseInt(JOptionPane.showInputDialog("Informe o valor de C : "));
             
-            //System.out.println("O Valor de Delta é: " +  delta);
-         
-            /// usando Bhaskara
-                if( delta > 0){
+            
+            
+            if(a == 0 ){
+                JOptionPane.showMessageDialog(null, "Execute novamente A = " + a);
+                System.exit(0);
 
-                    x1 = (-b + Math.sqrt(delta)) / (2 * a);
-                    x2 = (-b - Math.sqrt(delta)) / (2 * a);
-
-                    System.out.println("O valor de X1 é: " + x1);
-                    System.out.println("O valor de X1 é: " + x2);
-
-
-                }
-                
-
+            }
+          
+            delta = calcularDelta(a, b, c);
+            //delta = (b * b) - 4 * a * c;
+            
+            JOptionPane.showMessageDialog(null, delta);
+            
+            if(delta < 0) {
+                JOptionPane.showMessageDialog(null, "A equação não apresentará raízes reais");
+                System.exit(0);
+            }
+  
+           
+            x1 = (-b + Math.sqrt(delta)) / (2 * a);
+            x2 = (-b - Math.sqrt(delta)) / (2 * a);
+            
+            JOptionPane.showMessageDialog(null, "O Valor de Delta é: " +  delta);
+            JOptionPane.showMessageDialog(null, "O valor de X1 é: " + x1);
+            JOptionPane.showMessageDialog(null, "O valor de X2 é: " + x2);
+       
+           
+        } catch (Exception erro){
+            // pegando a exceção
+            //ex: tipo de dado inserido nas variaveis a , b e c;
+            JOptionPane.showMessageDialog(null, "insira informações corretas por favor.");
+            System.out.println(erro.getMessage());
+    
+        } finally {
+            JOptionPane.showMessageDialog(null, "Fim de Execução");
         }
-        
-    } 
+               
+    }
+    
+        private static double calcularDelta(int a, int b, int c) throws IllegalArgumentException{
+            double delta = (b * b) - 4 * a * c;
+        return delta;
+    }
     
 }
